5a41b020e87309f29de0aa1b266f66d2047feb4a
ahmedsayed1850 <khaledtoleo1850@gmail.com>
2022-04-04 21:52:34 +0200

    Simple Counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { actions } from "./store";
 
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  const increment = () => {
+    dispatch(actions.increment());
+  };
+  const decremenet = () => {
+    dispatch(actions.decrement());
+  };
+  const addBy = () => {
+    dispatch(actions.addBy(20));
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>counter</h1>
+      <button onClick={decremenet}>-</button>
+      <h2>{counter}</h2>
+      <button onClick={increment}>+</button>
+      <button onClick={addBy}>add By 10</button>
     </div>
   );
 }
